4c0743dad9831398e17b1976f8c94f82b157ebb3
Conrad Chase Metzger <chasemetzger15@gmail.com>
2019-01-29 21:26:18 -0800

    Comment out the createPages function, I don't need it yet

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,37 +1,37 @@
 const path = require('path');
 const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
 
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions;
+// exports.createPages = ({ actions, graphql }) => {
+//   const { createPage } = actions;
 
-  const indexPage = path.resolve('./src/pages/index.js');
+//   const indexPage = path.resolve('./src/pages/index.js');
 
-  return graphql(`
-    {
-      allMarkdownRemark {
-        edges {
-          node {
-            frontmatter {
-              path
-            }
-          }
-        }
-      }
-    }
-  `).then(result => {
-    if (result.errors) {
-      return Promise.reject(result.errors);
-    }
+//   return graphql(`
+//     {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             frontmatter {
+//               path
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `).then(result => {
+//     if (result.errors) {
+//       return Promise.reject(result.errors);
+//     }
 
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: node.frontmatter.path,
-        component: indexPage,
-        context: {},
-      });
-    });
-  });
-};
+//     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//       createPage({
+//         path: node.frontmatter.path,
+//         component: indexPage,
+//         context: {},
+//       });
+//     });
+//   });
+// };
 
 exports.onCreateWebpackConfig = ({
   stage,
