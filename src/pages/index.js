39e4a428597f743d37965fa4cd7b195b4ffec566
Conrad Chase Metzger <chasemetzger15@gmail.com>
2019-01-30 23:35:32 -0800

    Update custom api by 'returning' the nodes array directly rather than the whole github response

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react'; //, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Layout from 'components/layout';
 import Box from 'components/box';
@@ -8,14 +8,7 @@ import { useFetch } from '../common/hooks';
 import { graphql } from 'gatsby';
 
 const Index = ({ data }) => {
-  const { result } = useFetch('/.netlify/functions/myrepos', {});
-  const [repos, setRepos] = useState([]);
-
-  useEffect(() => {
-    if (result.data) {
-      setRepos(result.data.viewer.repositories.nodes);
-    }
-  }, [result]);
+  const { result } = useFetch('/.netlify/functions/myrepos', []);
 
   return (
     <Layout>
@@ -27,7 +20,7 @@ const Index = ({ data }) => {
         />
       </Box>
       <Gallery
-        items={repos.map(r => ({ title: r.name, description: r.description }))}
+        items={result.map(r => ({ title: r.name, description: r.description }))}
         title="My Github Projects"
       />
       <div style={{ height: '50vh' }} />
