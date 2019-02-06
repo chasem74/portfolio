4ccd8ac28b5f751a80594125917ae4def7ac58cf
Conrad Chase Metzger <chasemetzger15@gmail.com>
2019-02-05 22:37:01 -0800

    Dummy commit

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,37 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
-import posed from 'react-pose';
-import { Container } from './header.css';
+//import posed from 'react-pose';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+//import { Container } from './header.css';
 import Title from 'components/title';
-import Nav from 'components/header/nav';
+//import MainNav from 'components/header/nav';
 
 // Example of a component-specific page transition
-const AnimatedContainer = posed.div({
-  enter: {
-    y: 0,
-    transition: {
-      ease: 'easeInOut',
-    },
-  },
-  exit: {
-    y: '-100%',
-    transition: {
-      ease: 'easeInOut',
-    },
-  },
-});
+// const AnimatedContainer = posed.div({
+//   enter: {
+//     y: 0,
+//     transition: {
+//       ease: 'easeInOut',
+//     },
+//   },
+//   exit: {
+//     y: '-100%',
+//     transition: {
+//       ease: 'easeInOut',
+//     },
+//   },
+// });
 
 const Header = ({ title }) => (
-  <AnimatedContainer>
-    <Container>
+  <Navbar bg="dark" fixed="bottom">
+    <Navbar.Brand>
       <Link to="/">
         <Title as="h1">{title}</Title>
       </Link>
-
-      <Nav />
-    </Container>
-  </AnimatedContainer>
+    </Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link as="span">
+        <Link to="/about">About</Link>
+      </Nav.Link>
+      <Nav.Link as="span">
+        <Link to="/">Projects</Link>
+      </Nav.Link>
+    </Nav>{' '}
+    {/* <MainNav /> */}
+  </Navbar>
 );
 
 Header.propTypes = {
