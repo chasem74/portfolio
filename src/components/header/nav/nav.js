a5b8ce66cf94bca00b9d965197611b7e716795ec
Conrad Chase Metzger <cmetzger@bearcubs.santarosa.edu>
2019-02-07 21:12:02 -0800

    Implement UI for navigation bar; not finalized

--- src/components/header/nav/nav.js
+++ src/components/header/nav/nav.js
@@ -3,21 +3,29 @@ import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import Nav from 'react-bootstrap/Nav';
 
-//import { Container } from './nav.css';
+import { Container } from './nav.css';
+
+const SimpleNavLink = ({ to, label, as = 'li' }) => {
+  return (
+    <Nav.Link as={as} eventKey={to}>
+      <Link to={to}>{label}</Link>
+    </Nav.Link>
+  );
+};
+
+SimpleNavLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  as: PropTypes.string,
+};
 
 const MainNav = () => (
-  <Nav className="mr-auto">
-    <Nav.Link as="span">
-      <Link to="/about">About</Link>
-    </Nav.Link>
-    <Nav.Link as="span">
-      <Link to="/">Projects</Link>
-    </Nav.Link>
-  </Nav>
+  <Container>
+    <Nav className="mr-auto" variant="pills" as="ul">
+      <SimpleNavLink to="/about" label="About" />
+      <SimpleNavLink to="/" label="Projects" />
+    </Nav>
+  </Container>
 );
-
-MainNav.propTypes = {
-  title: PropTypes.string,
-};
 
 export default MainNav;
